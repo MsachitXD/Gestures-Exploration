b74ba754fe07d88b1ac1809451648a354de04779
Sachit Mittal <sachitmittal073@gmail.com>
2025-10-01 22:07:18 +0530

    Initial Commit

---
+++ Gestures Exploration/Gestures_ExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gestures_ExplorationApp.swift
+//  Gestures Exploration
+//
+//  Created by Sachit Mittal on 01/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Gestures_ExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gestures ExplorationTests/Gestures_ExplorationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gestures_ExplorationTests.swift
+//  Gestures ExplorationTests
+//
+//  Created by Sachit Mittal on 01/10/25.
+//
+
+import Testing
+@testable import Gestures_Exploration
+
+struct Gestures_ExplorationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
